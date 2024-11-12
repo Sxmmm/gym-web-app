3cda335c159fdcf067d40a6487f1aa562802ecc9
Sam Baker <09sam@live.co.uk>
2024-11-12 23:27:34 +0000

    change to trigger workflow

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const App: React.FC = () => {
     return (
         <div>
-            <h1>Hello, TypeScript with React and Webpack!</h1>
+            <h1>Hello, TypeScript with React and Webpack!!!</h1>
             <p>This is your TypeScript-based web app.</p>
         </div>
     );
