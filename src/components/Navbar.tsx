ca8695edfc4dbe06b37bb38bd39711fb8798d5a1
Sam Baker <09sam@live.co.uk>
2024-11-14 21:50:22 +0000

    fix attempt to for live refresh 404 error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,19 +9,19 @@ const Navbar: React.FC = () => {
             </div>
             <ul className="navbar-links">
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/gym-web-app">Home</Link>
                 </li>
                 <li>
-                    <Link to="/exercises">Exercises</Link>
+                    <Link to="/gym-web-app/exercises">Exercises</Link>
                 </li>
                 <li>
-                    <Link to="/planner">Planner</Link>
+                    <Link to="/gym-web-app/planner">Planner</Link>
                 </li>
                 <li>
-                    <Link to="/workouts">Pre Made Workouts</Link>
+                    <Link to="/gym-web-app/workouts">Pre Made Workouts</Link>
                 </li>
                 <li>
-                    <Link to="/about">About</Link>
+                    <Link to="/gym-web-app/about">About</Link>
                 </li>
             </ul>
         </nav>
